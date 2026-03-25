4ca1c38c9463ef0b6eaf254d6a5df1017833a234
sharyzadizain <kabanov.makap@yandex.ru>
2026-03-25 15:16:01 +0300

    Test2

--- tauri-app/src-tauri/src/lib.rs
+++ tauri-app/src-tauri/src/lib.rs
@@ -9,6 +9,16 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            tauri::WebviewWindowBuilder::new(
+                app,
+                "external",
+                tauri::WebviewUrl::External("https://web.tehnostrelka.kvantoriumvlz.ru/".parse().unwrap())
+            )
+            .build()?;
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
